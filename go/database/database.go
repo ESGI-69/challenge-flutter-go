fa6ca581e0951aa47bcad4b0e43925937c5ad46d
mrpinkcat <gatien.anizan@gmail.com>
2024-04-17 00:27:54 +0200

    fix: database connection error

--- go/database/database.go
+++ go/database/database.go
@@ -12,7 +12,8 @@ import (
 var database *gorm.DB
 
 func init() {
-	dsn := "postgres://" + config.GetConfig().DBUser + ":" + config.GetConfig().DBPassword + "@" + config.GetConfig().DBHost + ":" + config.GetConfig().DBPort + "/" + config.GetConfig().DBName
+	// dsn := "postgres://" + config.GetConfig().DBUser + ":" + config.GetConfig().DBPassword + "@" + config.GetConfig().DBHost + ":" + config.GetConfig().DBPort + "/" + config.GetConfig().DBName
+	dsn := "host=" + config.GetConfig().DBHost + " user=" + config.GetConfig().DBUser + " password=" + config.GetConfig().DBPassword + " dbname=" + config.GetConfig().DBName + " port=" + config.GetConfig().DBPort + " sslmode=disable"
 
 	var databaseConnectionError error
 	database, databaseConnectionError = gorm.Open(postgres.Open(dsn), &gorm.Config{})
