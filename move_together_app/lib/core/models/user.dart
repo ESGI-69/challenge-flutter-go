e57062f8c16ca15cfe1d294e170be2b3f71d05a1
Samuel GUENIER <guenier.samuel@gmail.com>
2024-05-28 15:15:59 +0200

    feat(register page) : register page + user model

---
+++ move_together_app/lib/core/models/user.dart
@@ -0,0 +1,23 @@
+class User {
+  final String id;
+  final String name;
+
+  User({
+    required this.id,
+    required this.name,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+}
--- move_together_app/lib/core/services/api_services.dart
+++ move_together_app/lib/core/services/api_services.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:move_together_app/core/models/user.dart';
 
 class ApiServices {
   static Future<String> loginUser(String username, String password) async {
@@ -20,6 +21,27 @@ class ApiServices {
       return token;
     } else {
       throw Exception('Failed to login user');
+    }
+  }
+
+  static Future<User> registerUser(String username, String password) async {
+    final response = await http.post(
+      Uri.parse('http://10.2.2:8080/register'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      final Map<String, dynamic> responseData = jsonDecode(response.body);
+      return User.fromJson(responseData);
+
+    } else {
+      throw Exception('Failed to register user');
     }
   }
 }
