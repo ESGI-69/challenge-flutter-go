07444422f9a7cda7f2d6f1e13aa7fd29a88a0e6a
mrpinkcat <gatien.anizan@gmail.com>
2024-05-29 18:49:01 +0200

    refacto(models): split model file

---
+++ go/models/transport.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type TransportType string
+
+const (
+	TransportTypeCar   TransportType = "car"
+	TransportTypePlane TransportType = "plane"
+	TransportTypeBus   TransportType = "bus"
+)
+
+type Transport struct {
+	gorm.Model
+	TransportType TransportType `gorm:"not null; default:'car'"`
+	StartDate     time.Time     `gorm:"not null"`
+	TripID        uint
+	Trip          Trip      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	EndDate       time.Time `gorm:"not null"`
+	StartAddress  string    `gorm:"not null"`
+	EndAddress    string    `gorm:"not null"`
+}
