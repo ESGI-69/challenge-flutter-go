16c41fd9f02d32be7e5d168b2d0369c75f9a79c6
mrpinkcat <gatien.anizan@gmail.com>
2024-06-06 01:25:44 +0200

    ci: flutter

--- move_together_app/lib/main.dart
+++ move_together_app/lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:move_together_app/app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load();
   runApp(const App());
 }
