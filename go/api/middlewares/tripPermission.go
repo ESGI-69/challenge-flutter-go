e225dc5238a551ceb89ddb35a0fb3844a566e049
mrpinkcat <gatien.anizan@gmail.com>
2024-06-09 15:46:53 +0200

    fix(middleware): abort if the trip is not found

--- go/api/middlewares/tripPermission.go
+++ go/api/middlewares/tripPermission.go
@@ -18,12 +18,16 @@ func init() {
 	databaseInstance = database.GetInstance()
 }
 
+// Check if the trip ID is correct, if the trip exist, and if the logged user is the owner of the trip
 func UserIsTripOwner(context *gin.Context) {
 	var trip models.Trip
-	var loggedUser models.User
-	retriveTripAndUser(context, &trip, &loggedUser)
+	var user models.User
+	err := retriveTripAndUser(context, &trip, &user)
+	if err {
+		return
+	}
 
-	if !trip.UserIsOwner(&loggedUser) {
+	if !trip.UserIsOwner(&user) {
 		context.AbortWithStatusJSON(401, gin.H{"error": "You need to be the owner of the trip to perform this action"})
 		return
 	}
@@ -31,10 +35,14 @@ func UserIsTripOwner(context *gin.Context) {
 	context.Next()
 }
 
+// Check if the trip ID is correct, if the trip exist, and if the logged user is the owner of the trip
 func UserHasTripEditRight(context *gin.Context) {
 	var trip models.Trip
 	var user models.User
-	retriveTripAndUser(context, &trip, &user)
+	err := retriveTripAndUser(context, &trip, &user)
+	if err {
+		return
+	}
 
 	if !trip.UserHasEditRight(&user) {
 		context.AbortWithStatusJSON(401, gin.H{"error": "You need to have the right to edit the trip to perform this action"})
@@ -47,7 +55,10 @@ func UserHasTripEditRight(context *gin.Context) {
 func UserIsTripParticipant(context *gin.Context) {
 	var trip models.Trip
 	var user models.User
-	retriveTripAndUser(context, &trip, &user)
+	err := retriveTripAndUser(context, &trip, &user)
+	if err {
+		return
+	}
 
 	if !trip.UserHasViewRight(&user) {
 		context.AbortWithStatusJSON(401, gin.H{"error": "You need to be a participant of the trip to perform this action"})
@@ -57,23 +68,23 @@ func UserIsTripParticipant(context *gin.Context) {
 	context.Next()
 }
 
-func retriveTripAndUser(context *gin.Context, trip *models.Trip, user *models.User) {
+func retriveTripAndUser(context *gin.Context, trip *models.Trip, user *models.User) bool {
 	currentUser, exist := utils.GetCurrentUser(context)
 	if !exist {
 		context.AbortWithStatusJSON(401, gin.H{"error": "Unauthorized"})
-		return
+		return true
 	}
 
 	tripId := context.Param("id")
 	if tripId == "" {
 		context.AbortWithStatusJSON(400, gin.H{"error": "Invalid trip ID"})
-		return
+		return true
 	}
 
 	tripIdUint, err := strconv.ParseUint(tripId, 10, 64)
 	if err != nil {
 		context.AbortWithStatusJSON(400, gin.H{"error": "Invalid trip ID"})
-		return
+		return true
 	}
 
 	*trip = models.Trip{Model: gorm.Model{ID: uint(tripIdUint)}}
@@ -81,8 +92,9 @@ func retriveTripAndUser(context *gin.Context, trip *models.Trip, user *models.Us
 	databaseErr := databaseInstance.Model(&trip).Preload(clause.Associations).First(&trip).Error
 	if databaseErr != nil {
 		errorHandlers.HandleGormErrors(databaseErr, context)
-		return
+		return true
 	}
 
 	*user = currentUser
+	return false
 }
