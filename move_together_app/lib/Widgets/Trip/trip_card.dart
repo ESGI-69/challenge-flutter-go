a861fead044478c658ae08b43b0492c51c2478da
mrpinkcat <gatien.anizan@gmail.com>
2024-06-13 18:42:23 +0200

    refacto(TripCard): Chage height factor to margin

--- move_together_app/lib/Widgets/Trip/trip_card.dart
+++ move_together_app/lib/Widgets/Trip/trip_card.dart
@@ -24,78 +24,78 @@ class TripCard extends StatelessWidget {
       onTap: onTap,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Stack(
-          children: [
-            Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(20)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: FractionallySizedBox(
-                heightFactor: 0.93,
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 40),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
                 child: Image.network(
                   imageUrl,
                   width: double.infinity,
                   fit: BoxFit.cover,
                 ),
               ),
-            ),
-            Positioned(
-              top: 10,
-              right: 10,
-              child: Row(
-                children: [
-                  Row(
-                    children: participants.map((participant) {
-                      return Container(
-                        margin: const EdgeInsets.only(right: 4),
-                        child: CircleAvatar(
-                          child: Center(
-                            child: Text(
-                              participant.username[0].toUpperCase(),
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
+              Positioned(
+                top: 10,
+                right: 10,
+                child: Row(
+                  children: [
+                    Row(
+                      children: participants.map((participant) {
+                        return Container(
+                          margin: const EdgeInsets.only(right: 4),
+                          child: CircleAvatar(
+                            child: Center(
+                              child: Text(
+                                participant.username[0].toUpperCase(),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                  const SizedBox(width: 10),
-                  Container(
-                    height: 40,
-                    width: 4,
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.2),
-                      borderRadius: const BorderRadius.all(Radius.circular(2)),
+                        );
+                      }).toList(),
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                  CircleAvatar(
-                    backgroundColor: Colors.black.withOpacity(0.2),
-                    child: const Icon(Icons.exit_to_app, color: Colors.white),
-                  ),
-                ],
+                    const SizedBox(width: 10),
+                    Container(
+                      height: 40,
+                      width: 4,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.2),
+                        borderRadius: const BorderRadius.all(Radius.circular(2)),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    CircleAvatar(
+                      backgroundColor: Colors.black.withOpacity(0.2),
+                      child: const Icon(Icons.exit_to_app, color: Colors.white),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            // Text at the Bottom
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: TripQuickInfo(
-                city: city,
-                date: date,
+              Center(
+                heightFactor: BorderSide.strokeAlignCenter,
+                child: TripQuickInfo(
+                  city: city,
+                  date: date,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
