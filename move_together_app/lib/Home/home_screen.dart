55a1ff8e74e7f604b001b53dfcf5e0f532b94ab3
mrpinkcat <gatien.anizan@gmail.com>
2024-06-13 18:03:48 +0200

    fix(Home): Change system UI text color for lisibility

--- move_together_app/lib/Home/home_screen.dart
+++ move_together_app/lib/Home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:move_together_app/Home/blocs/home_bloc.dart';
@@ -23,62 +24,67 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-          context.go('/join-trip');
-          },
-          child: const Icon(Icons.add),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0),
+        child: AppBar(
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+          backgroundColor: Colors.transparent,
         ),
-        body: BlocProvider(
-          create: (context) => HomeBloc()..add(HomeDataLoaded()),
-          child: BlocBuilder<HomeBloc, HomeState>(
-            builder: (context, state) {
-              if (state is HomeDataLoading) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (state is HomeDataLoadingError) {
-                return Center(
-                  child: Text(state.errorMessage),
-                );
-              } else if (state is HomeDataLoadingSuccess) {
-                if (state.trips.isEmpty) {
-                  return const EmptyHome();
-                }
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: Flex(
-                    direction: Axis.vertical,
-                    children: <Widget>[
-                    Expanded(
-                      child: PageView(
-                      onPageChanged: changePage,
-                      children: state.trips.map((trip) {
-                        return TripCard(
-                          onTap: () => context.push('/trip/${trip.id}', extra: trip),
-                          imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg/260px-Tour_Eiffel_Wikimedia_Commons.jpg",
-                          city: trip.city,
-                          date: trip.startDate.toString(),
-                          participants: trip.participants,
-                        );
-                      }).toList(),
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    PageIndicator(
-                      currentIndex: _currentIndex,
-                      pageCount: state.trips.length,
-                    ),
-                    ],
-                  ),
-                );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        context.go('/join-trip');
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: BlocProvider(
+        create: (context) => HomeBloc()..add(HomeDataLoaded()),
+        child: BlocBuilder<HomeBloc, HomeState>(
+          builder: (context, state) {
+            if (state is HomeDataLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is HomeDataLoadingError) {
+              return Center(
+                child: Text(state.errorMessage),
+              );
+            } else if (state is HomeDataLoadingSuccess) {
+              if (state.trips.isEmpty) {
+                return const EmptyHome();
               }
-              return const SizedBox();
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                  Expanded(
+                    child: PageView(
+                    onPageChanged: changePage,
+                    children: state.trips.map((trip) {
+                      return TripCard(
+                        onTap: () => context.push('/trip/${trip.id}', extra: trip),
+                        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg/260px-Tour_Eiffel_Wikimedia_Commons.jpg",
+                        city: trip.city,
+                        date: trip.startDate.toString(),
+                        participants: trip.participants,
+                      );
+                    }).toList(),
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  PageIndicator(
+                    currentIndex: _currentIndex,
+                    pageCount: state.trips.length,
+                  ),
+                  ],
+                ),
+              );
             }
-          )
-        ),
+            return const SizedBox();
+          }
+        )
       ),
     );
   }
