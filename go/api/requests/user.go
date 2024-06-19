6e9877bd270c2cdc7f361220615e7bd624dfab53
Samuel GUENIER <guenier.samuel@gmail.com>
2024-06-19 22:13:01 +0200

    fix(CreateBodyStruct): correct tag name

--- go/api/requests/user.go
+++ go/api/requests/user.go
@@ -1,6 +1,6 @@
 package requests
 
 type UserCreateBody struct {
-	Username string `json:"username" binding:"required" validate:"not_trimmed_empty"`
-	Password string `json:"password" binding:"required" validate:"min=8,max=64,not_trimmed_empty"`
+	Username string `json:"username" binding:"required" validate:"space_trimmed_empty"`
+	Password string `json:"password" binding:"required" validate:"min=8,max=64,space_trimmed_empty"`
 }
