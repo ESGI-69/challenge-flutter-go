be24f5d4b8a26d769b4ead2a9e5a255af386f024
Samuel GUENIER <guenier.samuel@gmail.com>
2024-06-23 16:04:01 +0200

    fix(Gorm Hanling errors): removing commentary

--- go/api/handlers/user.go
+++ go/api/handlers/user.go
@@ -97,11 +97,6 @@ func (handler *UserHandler) Create(context *gin.Context) {
 	user := models.User{Username: requestBody.Username, Password: requestBody.Password}
 	userCreationError := handler.Repository.Create(&user)
 	if userCreationError != nil {
-		// if strings.Contains(userCreationError.Error(), "uni_users_username") {
-		// 	context.JSON(http.StatusBadRequest, gin.H{"error": "Username already exists"})
-		// } else {
-		// 	errorHandlers.HandleGormErrors(userCreationError, context)
-		// }
 		errorHandlers.HandleGormErrors(userCreationError, context)
 		return
 	}
