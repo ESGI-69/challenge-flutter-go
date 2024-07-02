45f20d4ac24ef90f114f7ace6641d97c1db58007
mrpinkcat <gatien.anizan@gmail.com>
2024-07-02 15:52:00 +0200

    feat(transportCard): create transport row

--- move_together_app/lib/Transport/transport_row.dart
+++ move_together_app/lib/Transport/transport_row.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:move_together_app/core/models/transport.dart';
 
 class TransportRow extends StatelessWidget {
@@ -11,11 +12,43 @@ class TransportRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(transport.startAddress),
-        Text(transport.endAddress),
-      ],
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black12,
+          width: 1,
+        ),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 4,
+          horizontal: 8,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.directions_car,
+              color: Theme.of(context).primaryColor,
+              size: 18,
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Wrap(
+                children: [
+                  Text(
+                    "${transport.startAddress} - ${transport.endAddress}, ${DateFormat.yMMMd().format(transport.startDate)}",
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
+            const Icon(Icons.arrow_forward_ios, color: Colors.black38, size: 16)
+          ],
+        ),
+      ),
     );
   }
 }
