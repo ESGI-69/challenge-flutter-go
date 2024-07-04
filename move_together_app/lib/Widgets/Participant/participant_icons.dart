e01ff0ed324ef649d8e9816925fde085dcb51048
mrpinkcat <gatien.anizan@gmail.com>
2024-07-04 12:37:56 +0200

    feat(participantIcons): add + icon to list

--- move_together_app/lib/Widgets/Participant/participant_icons.dart
+++ move_together_app/lib/Widgets/Participant/participant_icons.dart
@@ -4,26 +4,52 @@ import 'package:move_together_app/core/models/participant.dart';
 
 class ParticipantIcons extends StatelessWidget {
   final List<Participant> participants;
+  final Function()? onTap;
 
   const ParticipantIcons({
     super.key,
     required this.participants,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 9),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: participants.map((participant) {
-          return Align(
-            widthFactor: 0.5,
-            child: ParticipantIcon(
-              participant: participant,
-            ),
-          );
-        }).toList(),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 9),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ...participants.map((participant) {
+              return Align(
+                widthFactor: 0.5,
+                child: ParticipantIcon(
+                  participant: participant,
+                ),
+              );
+            }),
+            onTap != null
+            ? Align(
+              widthFactor: 0.5,
+              child: Container(
+                height: 36,
+                width: 36,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+            )
+            : const SizedBox(),
+          ]
+        ),
       ),
     );
   }
