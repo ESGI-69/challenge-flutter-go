e47e14a559460d56e1d6383e10a2e7b8709f71c8
Samuel GUENIER <guenier.samuel@gmail.com>
2024-07-09 00:08:09 +0200

    fix(feature flipping): fix in json

--- go/api/requests/feature.go
+++ go/api/requests/feature.go
@@ -1,5 +1,5 @@
 package requests
 
 type FeatureUpdateBody struct {
-	IsEnabled bool `json:"enabled"`
+	IsEnabled bool `json:"isEnabled"`
 }
--- go/api/responses/feature.go
+++ go/api/responses/feature.go
@@ -4,7 +4,7 @@ import "challenge-flutter-go/models"
 
 type FeatureResponse struct {
 	Name      models.FeatureName `json:"name"`
-	IsEnabled bool               `json:"enabled"`
+	IsEnabled bool               `json:"isEnabled"`
 	ModifedBy UserResponse       `json:"modifedBy"`
 	UpdateAt  string             `json:"updateAt"`
 }
