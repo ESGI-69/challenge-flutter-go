e7aecfbadf79af86d24ea9324dccde9f40df2f35
Samuel GUENIER <guenier.samuel@gmail.com>
2024-07-08 21:49:32 +0200

    fix(feature flipping): add unique constraint on name of feature

--- go/models/feature.go
+++ go/models/feature.go
@@ -6,7 +6,7 @@ import (
 
 type Feature struct {
 	gorm.Model
-	Name         string `gorm:"not null;max:64"`
+	Name         string `gorm:"not null;unique;max:64"`
 	ModifiedByID uint
 	ModifiedBy   User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Enabled      bool
