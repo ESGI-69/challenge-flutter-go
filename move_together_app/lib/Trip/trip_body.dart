e64c77a678f27edc0882ca5a4262b101a486e814
Samuel GUENIER <guenier.samuel@gmail.com>
2024-07-14 15:16:08 +0200

    fix(feature flipping): adding condition on feature enabled

--- move_together_app/lib/Trip/trip_body.dart
+++ move_together_app/lib/Trip/trip_body.dart
@@ -7,6 +7,9 @@ import 'package:move_together_app/Transport/transport_card.dart';
 import 'package:move_together_app/Note/note_card.dart';
 import 'package:move_together_app/core/models/trip.dart';
 import 'package:move_together_app/Map/map_card.dart';
+import 'package:provider/provider.dart';
+import 'package:move_together_app/Provider/feature_provider.dart';
+import 'package:move_together_app/core/models/feature.dart';
 
 class TripBody extends StatelessWidget {
   final Trip trip;
@@ -20,42 +23,49 @@ class TripBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var featureProvider = Provider.of<FeatureProvider>(context);
     return ListView(
       children: [
         MapCard(tripId: trip.id),
-        TransportCard(
-          tripId: trip.id,
-          userHasEditPermission: trip.currentUserHasEditingRights(context),
-          userIsOwner: trip.isCurrentUserOwner(context),
-          onRefresh: onRefresh,
-        ),
-        AccommodationCard(
-          tripId: trip.id,
-          userHasEditPermission: trip.currentUserHasEditingRights(context),
-          userIsOwner: trip.isCurrentUserOwner(context),
-          onRefresh: onRefresh,
-        ),
-        PhotoCard(
-          tripId: trip.id,
-          userHasEditPermission: trip.currentUserHasEditingRights(context),
-          userIsOwner: trip.isCurrentUserOwner(context),
-        ),
-        DocumentCard(
-          tripId: trip.id,
-          userHasEditPermission: trip.currentUserHasEditingRights(context),
-          userIsOwner: trip.isCurrentUserOwner(context),
-        ),
-        NoteCard(
-          tripId: trip.id,
-          userHasEditPermission: trip.currentUserHasEditingRights(context),
-          userIsOwner: trip.isCurrentUserOwner(context),
-        ),
-        ActivityCard(
-          tripId: trip.id,
-          userHasEditPermission: trip.currentUserHasEditingRights(context),
-          userIsOwner: trip.isCurrentUserOwner(context),
-          onRefresh: onRefresh,
-        ),
+        if (featureProvider.isFeatureEnabled(FeatureNames.transport))
+          TransportCard(
+            tripId: trip.id,
+            userHasEditPermission: trip.currentUserHasEditingRights(context),
+            userIsOwner: trip.isCurrentUserOwner(context),
+            onRefresh: onRefresh,
+          ),
+        if (featureProvider.isFeatureEnabled(FeatureNames.accommodation))
+          AccommodationCard(
+            tripId: trip.id,
+            userHasEditPermission: trip.currentUserHasEditingRights(context),
+            userIsOwner: trip.isCurrentUserOwner(context),
+            onRefresh: onRefresh,
+          ),
+        if (featureProvider.isFeatureEnabled(FeatureNames.photo))
+          PhotoCard(
+            tripId: trip.id,
+            userHasEditPermission: trip.currentUserHasEditingRights(context),
+            userIsOwner: trip.isCurrentUserOwner(context),
+          ),
+        if (featureProvider.isFeatureEnabled(FeatureNames.document))
+          DocumentCard(
+            tripId: trip.id,
+            userHasEditPermission: trip.currentUserHasEditingRights(context),
+            userIsOwner: trip.isCurrentUserOwner(context),
+          ),
+        if (featureProvider.isFeatureEnabled(FeatureNames.note))
+          NoteCard(
+            tripId: trip.id,
+            userHasEditPermission: trip.currentUserHasEditingRights(context),
+            userIsOwner: trip.isCurrentUserOwner(context),
+          ),
+        if (featureProvider.isFeatureEnabled(FeatureNames.activity))
+          ActivityCard(
+            tripId: trip.id,
+            userHasEditPermission: trip.currentUserHasEditingRights(context),
+            userIsOwner: trip.isCurrentUserOwner(context),
+            onRefresh: onRefresh,
+          ),
       ],
     );
   }
