460d487b273dba39b466baf750f03d5c3966df5f
Samuel GUENIER <guenier.samuel@gmail.com>
2024-07-14 14:42:41 +0200

    fix(feature model): fix so the enum is used

--- move_together_app/lib/core/models/feature.dart
+++ move_together_app/lib/core/models/feature.dart
@@ -13,8 +13,62 @@ enum FeatureNames {
   activity,
 }
 
+extension FeatureNamesExtension on FeatureNames {
+  String get name {
+    switch (this) {
+      case FeatureNames.document:
+        return 'document';
+      case FeatureNames.auth:
+        return 'auth';
+      case FeatureNames.chat:
+        return 'chat';
+      case FeatureNames.trip:
+        return 'trip';
+      case FeatureNames.note:
+        return 'note';
+      case FeatureNames.transport:
+        return 'transport';
+      case FeatureNames.accommodation:
+        return 'accommodation';
+      case FeatureNames.user:
+        return 'user';
+      case FeatureNames.photo:
+        return 'photo';
+      case FeatureNames.activity:
+        return 'activity';
+    }
+  }
+
+  static FeatureNames fromName(String name) {
+    switch (name) {
+      case 'document':
+        return FeatureNames.document;
+      case 'auth':
+        return FeatureNames.auth;
+      case 'chat':
+        return FeatureNames.chat;
+      case 'trip':
+        return FeatureNames.trip;
+      case 'note':
+        return FeatureNames.note;
+      case 'transport':
+        return FeatureNames.transport;
+      case 'accommodation':
+        return FeatureNames.accommodation;
+      case 'user':
+        return FeatureNames.user;
+      case 'photo':
+        return FeatureNames.photo;
+      case 'activity':
+        return FeatureNames.activity;
+      default:
+        throw Exception('Unknown feature name: $name');
+    }
+  }
+}
+
 class Feature {
-  final String name;
+  final FeatureNames name;
   final bool isEnabled;
   final User modifiedBy;
   final DateTime updatedAt;
@@ -28,7 +82,7 @@ class Feature {
 
   factory Feature.fromJson(Map<String, dynamic> json) {
     return Feature(
-      name: json['name'],
+      name: FeatureNamesExtension.fromName(json['name']),
       isEnabled: json['isEnabled'],
       modifiedBy: User.fromJson(json['modifiedBy']),
       updatedAt: DateTime.parse(json['updatedAt']),
@@ -37,14 +91,14 @@ class Feature {
 
   Map<String, dynamic> toJson() {
     return {
-      'name': name,
+      'name': name.name,
       'isEnabled': isEnabled,
       'modifiedBy': modifiedBy.toJson(),
       'updatedAt': updatedAt.toIso8601String(),
     };
   }
 
-  copyWith({required bool isEnabled}) {
+  Feature copyWith({required bool isEnabled}) {
     return Feature(
       name: name,
       isEnabled: isEnabled,
